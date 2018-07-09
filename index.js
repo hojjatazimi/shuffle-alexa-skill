a17b99c285f872c41394f7d1d7c49f22f1130ed4
Hojjat <azimi.hojjat@yahoo.com>
2018-07-09 12:18:49 +0430

    done by Hojjat

--- index.js
+++ index.js
@@ -34,26 +34,28 @@ alexaApp.express({
 app.set("view engine", "ejs");
 
 alexaApp.launch(function(request, response) {
-  async function handler (){
-    const user_id = request.data.session.userId;
-    INDEXES[user_id] = 0;
-    const options = { method: 'GET',
-      url: 'https://streaming.shuffle.one/public/channel/promoted',
-      qs: { channelId: '889', page: '1', count: '200' },
-      headers: 
-       { 'Postman-Token': 'e7d28854-b797-46fd-8a34-3485aacf028c',
-         'Cache-Control': 'no-cache' } };
-    MUSICS[user_id] = request(options);
-    const music = MUSICS[user_id][INDEXES[user_id]];
-    const stream ={
-      "url": music.aacPath,
-      "token": music.id,
-      "offsetInMilliseconds": 0
-    };
-    response.say('Hi');
-    response.audioPlayerPlayStream("REPLACE_ALL", stream);
-  }
-  handler();
+  response.say('Hi');
+  // async function handler (){
+  //   const user_id = request.data.session.userId;
+  //   INDEXES[user_id] = 0;
+  //   const options = { method: 'GET',
+  //     url: 'https://streaming.shuffle.one/public/channel/promoted',
+  //     qs: { channelId: '889', page: '1', count: '200' },
+  //     headers: 
+  //      { 'Postman-Token': 'e7d28854-b797-46fd-8a34-3485aacf028c',
+  //        'Cache-Control': 'no-cache' } };
+  //   MUSICS[user_id] = request(options);
+  //   const music = MUSICS[user_id][INDEXES[user_id]];
+  //   const stream ={
+  //     "url": music.aacPath,
+  //     "token": music.id,
+  //     "offsetInMilliseconds": 0
+  //   };
+    
+    // response.audioPlayerPlayStream("REPLACE_ALL", stream);
+  // }
+  
+  // handler();
 });
 
 
