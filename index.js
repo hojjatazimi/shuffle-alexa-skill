3b268e0f9c73002b70894b34c0b6ed28b0d9b325
Hojjat <azimi.hojjat@yahoo.com>
2018-07-09 16:50:58 +0430

    done by Hojjat

--- index.js
+++ index.js
@@ -66,6 +66,9 @@ alexaApp.playbackController('NextCommandIssued', (req, response) => {
   };
   response.audioPlayerPlayStream("REPLACE_ALL", stream);
 });
+alexaApp.playbackController('PuseCommandIssued', (req, response)=>{
+  console.log('ammatono', req);
+})
 alexaApp.playbackController('PlaybackStopped', (req, response)=>{
   console.log('ammatono', req);
 })
