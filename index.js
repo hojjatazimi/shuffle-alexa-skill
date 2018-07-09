20198765243677d0a97fc60aeac8338348b85caf
Hojjat <azimi.hojjat@yahoo.com>
2018-07-09 13:26:06 +0430

    done by Hojjat

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ alexaApp.launch(async function(req, response) {
 alexaApp.audioPlayer("PlaybackStarted", function(request, response) {
   // immediate response
   console.log('HOJJAT_PLAY_BACK');
-  console.log(request.data.request.userId);
+  console.log(request.data);
   const user_id = request.data.request.userId;
   const prevMusic = MUSICS[user_id][INDEXES[user_id]];
   INDEXES[user_id] ++;
